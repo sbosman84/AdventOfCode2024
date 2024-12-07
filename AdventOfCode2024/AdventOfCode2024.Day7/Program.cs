05d0aa84e2adc016540716d4d72ba56b4f2f9e12
Sander Bosman <Sander.Bosman@giocoffee.nl>
2024-12-07 12:44:15 +0100

    Added first part of Day 7

--- AdventOfCode2024/AdventOfCode2024.Day7/Program.cs
+++ AdventOfCode2024/AdventOfCode2024.Day7/Program.cs
@@ -7,3 +7,52 @@ Console.WriteLine("--- Day 7: Bridge Repair ---");
 var lines = File.ReadAllLines(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "input.txt"));
 var stopwatch = new Stopwatch();
 stopwatch.Start();
+
+long totalCalibrationResult = 0;
+
+foreach (var line in lines)
+{
+    var parts = line.Split(':');
+
+    long testValue = long.Parse(parts[0].Trim());
+    var numberStrings = parts[1].Trim().Split(' ');
+    var numbers = Array.ConvertAll(numberStrings, int.Parse);
+
+    // Generate all possible combinations of '+' and '*' operators
+    int operatorCount = numbers.Length - 1;
+    int totalCombinations = 1 << operatorCount;
+
+    for (int combination = 0; combination < totalCombinations; combination++)
+    {
+        long result = numbers[0];
+
+        for (int i = 0; i < operatorCount; i++)
+        {
+            int currentNumber = numbers[i + 1];
+            int operatorBit = (combination >> i) & 1;
+
+            if (operatorBit == 0)
+            {
+                // '+' operator
+                result += currentNumber;
+            }
+            else
+            {
+                // '*' operator
+                result *= currentNumber;
+            }
+        }
+
+        if (result == testValue)
+        {
+            totalCalibrationResult += testValue;
+            break;
+        }
+    }
+}
+
+Console.WriteLine($"Total calibration result: {totalCalibrationResult}");
+
+stopwatch.Stop();
+Console.WriteLine($"Calculation performed in: {stopwatch.ElapsedMilliseconds} ms");
+Console.ReadKey();
